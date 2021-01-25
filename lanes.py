031e0b1c5ac805b05ab1fdb1c34f9680dc977c72
priyan-coder <rajamohanpriyan@gmail.com>
2021-01-25 11:58:24 +0800

    found the straight lines in the image

--- lanes.py
+++ lanes.py
@@ -12,9 +12,8 @@ def cannyAlgo(img):
     canny = cv2.Canny(blur, 50, 150)
     return canny
 
+
 # returns the enclosed region of field of view
-
-
 def regionOfInterest(img):
     heightOfImage = img.shape[0]
     polygons = np.array([
@@ -26,6 +25,26 @@ def regionOfInterest(img):
     return masked_image
 
 
+def displayLines(img, linesDetectedInGradientImage):
+    blackImgThatFollowsShapeOfimg = np.zeros_like(img)
+
+    # if lines are detected
+    if linesDetectedInGradientImage is not None:
+        # each line is a 2d array
+        for line in linesDetectedInGradientImage:
+            print(line)
+            # reshape into a 1-d array
+            x1, y1, x2, y2 = line.reshape(4)
+            # draw this line that join the coordinates
+            # on the black image which traces the parent image
+            # with the BGR values (255,0,0)
+            # and 10 px thickness
+            cv2.line(blackImgThatFollowsShapeOfimg,
+                     (x1, y1), (x2, y2), (255, 0, 0), 10)
+
+    return blackImgThatFollowsShapeOfimg
+
+
 # loads the image
 # returns a multi - dimensional np array with the
 # relative intensities of each pixel of the image
@@ -35,12 +54,41 @@ image = cv2.imread("test_image.jpg")
 # copy the array into a new variable
 lane_image = np.copy(image)
 
+# image wih pixel axis
 canny = cannyAlgo(lane_image)
+
+# displays only region of interest
 cropped_image = regionOfInterest(canny)
 
 
+# lines ==> detected straight lines on image
+# 1st argument ==> image in which you want to detect line
+# 2nd (p) and 3rd (teta) agruments specify the resolution of the
+# Hough accumulator array or the grid which is a 2d array
+# which we use to collect votes for the best fit line
+# Each bin or grid has a distinct p, in pixels and teta value in radians
+# 4th argument ==> we specify threshold on which bin to choose
+# i.e. min num of votes to detect a line
+# we choose the bin with >=100 votes
+# 5th argument is a placeholder array for the output
+# Any detected line less than 40 pixels is rejected
+# Between segmented lines detected on image analysed, if the gap is <= 5,
+# we join them together
+lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100,
+                        np.array([]), minLineLength=40, maxLineGap=5)
+
+line_image = displayLines(lane_image, lines)
+
+# addWeighted takes the sum of the color lane_image
+# and the line_image which has the lines
+# Multiply all pixel intensities in lane_image by 0.8
+# this makes it darker
+# multiply line_image by 1
+# add 1 to the sum of the pixel intensities of the two images
+comboImage = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
+
 # show the image
-cv2.imshow("result", cropped_image)
+cv2.imshow("result", comboImage)
 
 # displays the image for a specified amt of milli seconds
 # setting it to 0 ==> makes the image stay displayed
@@ -48,7 +96,7 @@ cv2.imshow("result", cropped_image)
 cv2.waitKey(0)
 
 
-# THE LOGIC BEHIND THE CODE
+# ---------------------THE LOGIC BEHIND THE CODE------------------
 
 # conda install -c conda-forge opencv
 # check pip installation online
@@ -105,3 +153,35 @@ cv2.waitKey(0)
 # The mask we created is completely filled with 1s
 # With the bitwise & pixel by pixel ==> we chop off other edges
 # and retain just the path we are interested in
+
+
+# 6. Detecting Straight lines in an image ==> Hough transform
+# eqn of a straight line ==> y = mx + b
+# In hough space, we plot b against m
+# A single x,y coordinate point
+# can be passed through by many lines with varying b,m
+# So hough space represents possible combinations of b.m as a line
+# for each x,y coordinate point in the cartesian space
+# Interestion point between 2 lines in hough space represents
+# b,m combination line that passes through the two
+# points x1,y1 and x2, y2
+# But again there can be multiple lines with other b,m combinations
+# passing through x1, y1 and x2, y2
+# To choose the best fit line to pass through the points
+# we make grids in the hough space and
+# the grid with the most number of intersection point is
+# the one from which we choose a possible b,m combination
+# for the best fit line
+# A vote is cast for each point of intersection
+# Vertical lines can pass through a x,y coordinate point
+# But that has m = infinity
+# This, we cant represent in hough space
+# Thus instead of y = mx + b for lines
+# We adopt expression of a line using polar coordinates
+# where p, called as rho, is the perpendicular distance
+# from the origin to the line or point on the line and
+# teta is the angle of inclination from the positive x axis to the rho line
+# p = xcos(teta) + gsin(teta)
+# where x, g is the coordinates of a point
+# So in the hough space, we now plot the possible (p, teta)
+# for each point in the cartesian space
